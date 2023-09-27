6c96e344acde498ffada80b534a220d5a92c4601
schaedejo00 <j.schaede@gymnasium-oberstadt.de>
2023-09-27 08:40:32 +0200

    bugfix

--- exampleCeasar.hs
+++ exampleCeasar.hs
@@ -1,15 +1,17 @@
 import Data.Char          --Import für die Funktionen chr und ord
 
-encryptText :: [Char] -> Int -> [Char]
-encryptText text secret = map charCaesar text
+encryptText :: [Char] -> Int -> [Char]    --ML
+encryptText text secret = map (encryptCharSecret secret) text
 
-encryptCharSecret:: Char -> Int -> Char
-encryptCharSecret char secret = chr (65 + 
+charCaesar:: Char -> Char                
+charCaesar char = encryptCharSecret 1 char
+
+encryptCharSecret:: Int -> Char -> Char
+encryptCharSecret secret txt = chr (65 + 
                            (  
-                            ((ord char) + secret - 65) 
+                            ((ord txt) + secret - 65) 
                            `mod` 26) 
                                      )
                    
-charCaesar:: Char -> Char
-charCaesar char = encryptCharSecret char 1
+
 
